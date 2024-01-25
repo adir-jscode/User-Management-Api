c24a103cda2ced32d84c836f25ca4c67a8cd3510
Adir <adir.jscode@gmail.com>
2024-01-26 00:58:43 +0600

    Log Entity Added

---
+++ UserManagementDashboardApi/Core/Entities/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace UserManagementDashboardApi.Core.Entities
+{
+    public class BaseEntity<TID>
+    {
+        public TID Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt {  get; set; }
+        public bool IsActive { get; set; } = true;
+        public bool IsDeleted { get; set; } = false;
+    }
+}
