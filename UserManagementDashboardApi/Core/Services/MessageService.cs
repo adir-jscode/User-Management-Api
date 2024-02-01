d26e769945999c956328d43def11e58fcbe57987
Adir <adir.jscode@gmail.com>
2024-02-01 18:16:19 +0600

    Message Service onGoing

--- UserManagementDashboardApi/Core/Services/MessageService.cs
+++ UserManagementDashboardApi/Core/Services/MessageService.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
 using System.Security.Claims;
 using UserManagementDashboardApi.Core.DbContext;
 using UserManagementDashboardApi.Core.Dtos.General;
@@ -15,86 +16,55 @@ namespace UserManagementDashboardApi.Core.Services
         private readonly ILogService _logService;
         private readonly UserManager<ApplicationUser> _userManager;
 
-        public MessageService(ApplicationDbContext context, ILogService logService, UserManager<ApplicationUser> userManager)
-        {
-            _context = context;
-            _logService = logService;
-            _userManager = userManager;
-        }
-
         public async Task<GeneralServiceResponseDto> CreateNewMessageAsync(ClaimsPrincipal User, CreateMessageDto createMessageDto)
         {
-           if(User.Identity.Name == createMessageDto.ReceiverUserName)
+            if (User.Identity.Name == createMessageDto.ReceiverName)
+            { 
                 return new GeneralServiceResponseDto()
                 {
                     IsSucceed = false,
                     StatusCode = 400,
-                    Message = "Sender and Receiver can not be same",
+                    Message = "User Can't be same"
                 };
-
-            var isReceiverUserNameValid = _userManager.Users.Any(q => q.UserName == createMessageDto.ReceiverUserName);
-            if(!isReceiverUserNameValid)
-                return new GeneralServiceResponseDto()
-                {
-                    IsSucceed = false,
-                    StatusCode = 400,
-                    Message = "Receiver UserName is not valid",
-                };
-
+            }
+            //var IsValidUser = _userManager.Users.Any(User.Identity.Name == createMessageDto.ReceiverName);
+            //if(!IsValidUser)
+            //{
+            //    return new GeneralServiceResponseDto()
+            //    {
+            //        IsSucceed = false,
+            //        StatusCode = 400,
+            //        Message = "User is not found"
+            //    };
+            //}
             Message newMessage = new Message()
             {
                 SenderUserName = User.Identity.Name,
-                ReceiverUserName = createMessageDto.ReceiverUserName,
-                Text = createMessageDto.Text
+                ReceiverUserName = createMessageDto.ReceiverName,
+                Text = createMessageDto.Text,
             };
-            await _context.Messages.AddAsync(newMessage);
+            await _context.AddAsync(newMessage);
             await _context.SaveChangesAsync();
-            await _logService.SaveNewLog(User.Identity.Name, "Send Message");
+
+
 
             return new GeneralServiceResponseDto()
             {
                 IsSucceed = true,
                 StatusCode = 201,
-                Message = "Message saved successfully",
+                Message = "Message sent Successfully"
             };
         }
 
-        public async Task<IEnumerable<GetMessageDto>> GetMessagesAsync()
+        public Task<IEnumerable<GetMessageDto>> GetMessagesAsync()
         {
-            var messages = await _context.Messages
-                .Select(q => new GetMessageDto()
-                {
-                    Id = q.Id,
-                    SenderUserName = q.SenderUserName,
-                    ReceiverUserName = q.ReceiverUserName,
-                    Text = q.Text,
-                    CreatedAt = q.CreatedAt
-                })
-                .OrderByDescending(q => q.CreatedAt)
-                .ToListAsync();
-
-            return messages;
+            throw new NotImplementedException();
         }
 
-        public async Task<IEnumerable<GetMessageDto>> GetMyMessagesAsync(ClaimsPrincipal User)
+        public Task<IEnumerable<GetMessageDto>> GetMyMessagesAsync(ClaimsPrincipal User)
         {
-            var loggedInUser = User.Identity.Name;
-
-            var messages = await _context.Messages
-                .Where(q => q.SenderUserName == loggedInUser || q.ReceiverUserName == loggedInUser)
-             .Select(q => new GetMessageDto()
-             {
-                 Id = q.Id,
-                 SenderUserName = q.SenderUserName,
-                 ReceiverUserName = q.ReceiverUserName,
-                 Text = q.Text,
-                 CreatedAt = q.CreatedAt
-             })
-             .OrderByDescending(q => q.CreatedAt)
-             .ToListAsync();
-
-            return messages;
+            throw new NotImplementedException();
         }
     }
-    }
+}
 
